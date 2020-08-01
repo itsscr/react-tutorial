2d183145839dc8a442e5e4f35e42538860636f45
itsscr <stkrch@gmail.com>
2020-08-01 12:52:41 +0530

    my first react appliction

--- ch1/01-react/src/App.js
+++ ch1/01-react/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <img className="avatar" src="https://themes.2the.me/Messenger-1.1/assets/images/avatars/12.jpg" alt=""/>
-      <h1>santosh chauhan male</h1>
+      <h1>santosh chauhan male</h1>d
     </div>
   );
 }
