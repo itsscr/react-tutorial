8d2947f1b9a74afea0bc95f59c7e22e906a53825
itsscr <stkrch@gmail.com>
2020-08-01 12:48:32 +0530

    my first react appliction

---
+++ ch1/01-react/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img className="avatar" src="https://themes.2the.me/Messenger-1.1/assets/images/avatars/12.jpg" alt=""/>
+      <h1>santosh chauhan male</h1>
+    </div>
+  );
+}
+
+export default App;
